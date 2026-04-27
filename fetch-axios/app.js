1dc976b42c658879a1c9e17c5763f318a527eb42
manueljose199-spec <manueljoseemma199@gmail.com>
2026-04-26 18:39:11 -0600

    fix:se corrigieron errones para captura de datos

--- fetch-axios/app.js
+++ fetch-axios/app.js
@@ -1,52 +1,54 @@
-const btnFetch=document.getElementById('btnFetch');
-const btnAxios=document.getElementById('btnAxios');
-const container=document.getElementById('data-container');
+const btnFetch = document.getElementById('btnFetch');
+const btnAxios = document.getElementById('btnAxios');
+const container = document.getElementById('data-container');
 const titulo = document.getElementById('titulo');
+
 const API_URL = 'https://rickandmortyapi.com/api/character';
 
-//  FETCH
-
-btnFetch.addEventListener('click',async()=>{
-    try{
-        titulo.textContent = 'Personajes Rick & Morty (Fetch)';
-        const res=await fetch(API_URL);
-        const data=await res.json();
-        mostrarPersonajes(data.results);
-
-
-    }catch(error){
-        console.log('error fetch',error);
-    }
-});
-
-
-//MOSTRAR DATOS
-
-btnAxios.addEventListener('click', async () => {
+// FETCH
+btnFetch.addEventListener('click', async () => {
   try {
-    titulo.textContent = 'Personajes Rick & Morty (Axios)';
-    const res = await axios.get(API_URL);
-    mostrarPersonajes(res.data.results);
+    titulo.textContent = 'Personajes Rick & Morty (Fetch)';
+    
+    const res = await fetch(API_URL);
+    if (!res.ok) throw new Error('Error en la API');
+
+    const data = await res.json();
+    mostrarPersonajes(data.results);
+
   } catch (error) {
-    console.log('error en axios', error);
+    console.error(error);
+    container.textContent = 'Error al cargar datos con Fetch';
   }
 });
 
+// AXIOS
+btnAxios.addEventListener('click', async () => {
+  try {
+    titulo.textContent = 'Personajes Rick & Morty (Axios)';
 
-//  MOSTRAR DATOS
+    const res = await axios.get(API_URL);
+    mostrarPersonajes(res.data.results);
 
-function mostrarPersonajes(personajes){
-    container.innerHTML='';
+  } catch (error) {
+    console.error(error);
+    container.textContent = 'Error al cargar datos con Axios';
+  }
+});
 
-    personajes.forEach(p=> {
-        const card=document.createElement('div');
-        card.classList.add('card');
+// RENDER
+function mostrarPersonajes(personajes) {
+  container.innerHTML = '';
 
-        card.innerHTML=`
-        <img src="${p.image}"/>
-        <h4>${p.name}<h4/>`
+  personajes.forEach(p => {
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-        container.appendChild(card);
-        
-    });
+    card.innerHTML = `
+      <img src="${p.image}" />
+      <h4>${p.name}</h4>
+    `;
+
+    container.appendChild(card);
+  });
 }
